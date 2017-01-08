2e0afa173660bf78797ca1795e4d5279f6db6cea
Ted Mader <tlmader.dev@gmail.com>
2017-01-07 19:10:28 -0600

    Implement Quicksort

--- QuickSort.java
+++ QuickSort.java
@@ -5,6 +5,7 @@ public class QuickSort {
 
   private static Scanner input;
   private static boolean debug = true;
+  private static Random rnd;
 
   public static void testCase(int x) {
     String[] strs = input.nextLine().split(" ");
@@ -12,11 +13,39 @@ public class QuickSort {
     for (int i = 0; i < nums.length; i++) {
       nums[i] = Integer.parseInt(strs[i]);
     }
-    nums = quickSort(nums);
+    nums = quickSort(nums, 0, nums.length - 1);
     printCase(x, Arrays.toString(nums));
   }
 
-  public static int[] quickSort(int[] nums) {
+  public static int[] quickSort(int[] nums, int low, int high) {
+    if (nums == null || nums.length == 0) {
+      return null;
+    }
+    int pivot = rnd.nextInt(high - low) + low + 1;
+    int i = low;
+    int j = high;
+    while (i <= j) {
+      while (nums[i] < pivot) {
+        i++;
+      }
+      while (nums[j] > pivot) {
+        j--;
+      }
+      if (i <= j) {
+          int temp = nums[i];
+          nums[i] = nums[j];
+          nums[j] = temp;
+          log(nums[i] + " <-> " + nums[j] + ": " + Arrays.toString(nums));
+          i++;
+          j--;
+      }
+    }
+    if (low < j) {
+      nums = quickSort(nums, low, j);
+    }
+    if (i < high) {
+      nums = quickSort(nums, i, high);
+    }
     return nums;
   }
 
@@ -30,7 +59,7 @@ public class QuickSort {
 
   public static void log(String message) {
     if (debug) {
-      System.out.println(message);
+      System.out.println("LOG: " + message);
     }
   }
 
@@ -42,6 +71,7 @@ public class QuickSort {
 
   public static void main(String[] args) {
     input = new Scanner(System.in);
+    rnd = new Random();
     int x = parseIntFromLine();
     for (int i = 1; i <= x; i++) {
       testCase(i);
